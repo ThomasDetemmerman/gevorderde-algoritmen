64e630ed0b9f4a1e4412deb683ee0f388fb3a52a
Thomas <detemmerman.thomas@hotmail.com>
2018-09-30 12:44:38 +0200

    rotate input

--- Labo1 - Rood-Zwarte bomen/main3.cpp
+++ Labo1 - Rood-Zwarte bomen/main3.cpp
@@ -5,18 +5,33 @@
 using std::cout;
 using std::vector;
 
+void printRepOK(RZWboom<int> & zkbm){
+    cout << (zkbm.repOK() ? "Voldoet aan opgelegde voorwaarden\n" : "Voldoet niet aan opgelegd voorwaarden  (inorder + ptr naar ouder)\n");  
+};
+
 int main()
 {
-    const vector<int> smallTree({10, 3, 15, 2, 6, 12, 18, 4, 8, 11, 13, 16, 20});
+    //const vector<int> smallTree({10, 3, 15, 2, 6, 12, 18, 4, 8, 11, 13, 16, 20});
+    const vector<int> smallTree({5,2,10,8,12,6,9});
+   
     RZWboom<int> zkbm;
 
     for (int i = 0; i < smallTree.size(); i++)
     {
         zkbm.voegtoe(smallTree[i]);
     }
-    zkbm.tekenAlsBinaireBoom("smallTree.dot");
+   
+    // Start
+    zkbm.tekenAlsBinaireBoom("default_smallTree.dot");
+    cout << "\nfile generated: default_smallTree.dot\n";
+    printRepOK(zkbm);
 
-    cout << (zkbm.repOK() ? "Voldoet aan opgelegde voorwaarden\n" : "Voldoet niet aan opgelegd voorwaarden  (inorder + ptr naar ouder)\n");
-    cout << "\noef 1: file generated\n";
-    cout << "Please copy content of the created file here: http://webgraphviz.com\n";
+    // roteer rechts
+    cout << "\nRotating smallTree to the right\n";
+    zkbm.roteer(true);
+    zkbm.tekenAlsBinaireBoom("rotate_right_smallTree.dot");
+    cout << "\nfile generated: rotate_right_smallTree.dot\n";
+    printRepOK(zkbm);
+
+    cout << "\nPlease copy content of the created file here: http://webgraphviz.com\n";
 }
