3ec90564b5b3d5f30aede023067bf521c0678807
Thomas <detemmerman.thomas@hotmail.com>
2018-09-30 12:55:32 +0200

    update smallTree array

--- Labo1 - Rood-Zwarte bomen/main3.cpp
+++ Labo1 - Rood-Zwarte bomen/main3.cpp
@@ -11,8 +11,8 @@ void printRepOK(RZWboom<int> & zkbm){
 
 int main()
 {
-    //const vector<int> smallTree({10, 3, 15, 2, 6, 12, 18, 4, 8, 11, 13, 16, 20});
-    const vector<int> smallTree({5,2,10,8,12,6,9});
+    const vector<int> smallTree({10, 3, 15, 2, 6, 12, 18, 4, 8, 11, 13, 16, 20});
+    //const vector<int> smallTree({5,2,10,8,12,6,9});   //werkt enkel met een left-rotate
    
     RZWboom<int> zkbm;
 
@@ -28,7 +28,7 @@ int main()
 
     // roteer rechts
     cout << "\nRotating smallTree to the right\n";
-    zkbm.roteer(true);
+    zkbm.roteer(false);
     zkbm.tekenAlsBinaireBoom("rotate_right_smallTree.dot");
     cout << "\nfile generated: rotate_right_smallTree.dot\n";
     printRepOK(zkbm);
