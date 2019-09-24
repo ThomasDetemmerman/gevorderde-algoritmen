e8372b0ad9229e50f0b1bb87a12ffebefc6c2ab3
Thomas <thomas.detemmerman@ugent.be>
2019-09-24 22:38:59 +0200

    balance tree after add

--- 2019-2020/1.1 Rood-Zwart boom bottom-up/main.cpp
+++ 2019-2020/1.1 Rood-Zwart boom bottom-up/main.cpp
@@ -3,30 +3,27 @@
 #include <iostream>
 #include <unistd.h>
 
-using std::vector;
 using std::cout;
 using std::endl;
-
+using std::vector;
 
 int main()
 {
 
-    vector<int> keys =    {50,  30, 100,  20, 40,   110, 10,   49,  105, 120,  3,  1 ,   4};
-    vector<char> values = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',  'j', 'h', 'i', 'j'};
+    vector<int> keys = {50, 30, 100, 20, 40, 110, 10, 49, 105, 120, 3, 1, 4};
+    vector<char> values = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'h', 'i', 'j'};
 
-    vector<int> keys_small =    {50, 30, 100, 20, 40};
+    vector<int> keys_small = {50, 30, 100, 20, 40};
     vector<char> value_small = {'a', 'b', 'c', 'd', 'k'};
 
     Zoekboom<int, char> zkbm;
     for (int i = 0; i < keys.size(); i++)
     {
         zkbm.voegtoe(keys[i], values[i]);
+        zkbm.teken(("input"+std::to_string(i)+".dot").c_str());
     }
-    zkbm.teken("output_before.dot");
-    cout << "Diepte van deze boom is " << zkbm.geefDiepte() << endl;
-    zkbm.roteer(true);
-    zkbm.maakOnevenwichtig();
-    zkbm.teken("onevenwichtig.dot");
+
+
 
     // convert dot files to png.
     execl("./dot2png.sh", "./dot2png.sh", NULL);
--- 2019-2020/1.1 Rood-Zwart boom bottom-up/zoekboom17.h
+++ 2019-2020/1.1 Rood-Zwart boom bottom-up/zoekboom17.h
@@ -1,21 +1,21 @@
 #ifndef __Zoekboom_H
 #define __Zoekboom_H
 #include <cstdlib>
-#include <iostream>
 #include <queue>
 #include <memory>
 #include <functional>
 #include <fstream>
 #include <sstream>
-
 #include <iostream>
+
+#include <stack>
 #include "zoekknoop.h"
 
-using std::cerr;
 using std::ofstream;
 using std::ostream;
 using std::ostringstream;
 using std::pair;
+using std::stack;
 using std::string;
 using std::unique_ptr;
 /**********************************************************************
@@ -45,13 +45,6 @@ public:
     // constructoren met zoekknoop als parameter
     // dit is de belangrijkste constructor van deze oefening. Het zet een pointer naar een zoekKnoop om naar een zoekboom.
     Zoekboom(unique_ptr<zoekKnoop<Sleutel, Data>> &&k) : unique_ptr<zoekKnoop<Sleutel, Data>>(move(k)){}; //move 2
-    /*
-    Unique pointers kun je niet kopieeren. 
-    copy operatoren kunnen echter wel zinvol zijn op voorwaarde dat je een deep copy implementeerd en dus de volledige boom kopieerd.
-
-    Zoekboom(const unique_ptr<zoekKnoop<Sleutel, Data>> &k) : unique_ptr<zoekKnoop<Sleutel, Data>>(k){};  //copy 2
-    Zoekboom &operator=(const unique_ptr<zoekKnoop<Sleutel, Data>> &a){return Zoekboom(a); };             //copy assingment operator required by voegtoe
-    */
 
     //destructor
     virtual ~Zoekboom() = default;
@@ -62,21 +55,27 @@ public:
     string tekenrec(ostream &uit, int &knoopteller);
 
     //te implementeren
-    bool repOK() const;
+   
     int geefDiepte();
     // geefBoomBovenKnoop: gegeven een knooppointer, wele boom wijst naar de knoop
     // preconditie: knoop moet een naar een geldige knoop wijzen.
     Zoekboom<Sleutel, Data> *geefBoomBovenKnoop(zoekKnoop<Sleutel, Data> &knoopptr);
     void voegtoe(const Sleutel &sleutel, const Data &data, bool dubbelsToestaan = false);
-
-    //eigen
+    
+private:
     void roteer(bool naarRechts);
+    bool repOK() const;
+    bool repOK_redParentWithBlackChilds() const;
+    bool repOK_blackRoot() const;
+    void fixBoom(stack<Zoekboom<Sleutel, Data> *> voorouders);
+    bool isLeftChildOfParent( Zoekboom<Sleutel, Data> &p);
+    Kleur getColorOfBrother(Zoekboom<Sleutel, Data> &p);
 
 protected:
     //zoekfunctie zoekt sleutel en geeft de boom in waaronder de sleutel zit (eventueel een lege boom als de sleutel
     //ontbreekt) en de pointer naar de ouder (als die bestaat, anders nulpointer).
     //noot: alhoewel de functie niets verandert aan de boom is ze geen const functie.
-    void zoek(const Sleutel &sleutel, zoekKnoop<Sleutel, Data> *&ouder, Zoekboom<Sleutel, Data> *&plaats);
+    stack<Zoekboom<Sleutel, Data> *> zoek(const Sleutel &sleutel, zoekKnoop<Sleutel, Data> *&ouder, Zoekboom<Sleutel, Data> *&plaats);
 };
 
 /*****************************************************************************
@@ -164,9 +163,10 @@ Zoekboom<Sleutel, Data> *Zoekboom<Sleutel, Data>::geefBoomBovenKnoop(zoekKnoop<S
 template <class Sleutel, class Data>
 void Zoekboom<Sleutel, Data>::voegtoe(const Sleutel &sleutel, const Data &data, bool dubbelsToestaan)
 {
+    
     zoekKnoop<Sleutel, Data> *ouder;
     Zoekboom<Sleutel, Data> *plaats;
-    Zoekboom<Sleutel, Data>::zoek(sleutel, ouder, plaats);
+    stack<Zoekboom<Sleutel, Data> *> voorouders = zoek(sleutel, ouder, plaats);
     if (dubbelsToestaan)
         while (*plaats)
             (*plaats)->geefKind(random() % 2).zoek(sleutel, ouder, plaats);
@@ -177,24 +177,175 @@ void Zoekboom<Sleutel, Data>::voegtoe(const Sleutel &sleutel, const Data &data, 
         nieuw->ouder = ouder;
         *plaats = move(nieuw);
     }
+    std::cout << "wij voegen toe: " << sleutel << std::endl;
+    fixBoom(voorouders);
 }
 
+/*
+*   Deze functie kan gezien worden als een verlengde van voegToe. Om de code van voegToe niet te onoverzichtelijk
+*   te maken is de code om de boom te repareren na toevoegen in een apparte functie gestoken.
+*/
 template <class Sleutel, class Data>
-void Zoekboom<Sleutel, Data>::zoek(const Sleutel &sleutel, zoekKnoop<Sleutel, Data> *&ouder, Zoekboom<Sleutel, Data> *&plaats)
+void Zoekboom<Sleutel, Data>::fixBoom(stack<Zoekboom<Sleutel, Data> *> voorouders)
 {
+    // Situatie 0: de wortel mag altijd zwart gemaakt worden
+    // empty slaat op een boom met maar 1 knoop.
+    if(voorouders.size() == 1 || voorouders.empty()){
+        (*this)->kleur = Kleur::Zwart;
+        if (voorouders.size() == 1) {
+            voorouders.pop();
+        }
+        return;
+    }
+    
+    if(voorouders.size()< 2){
+        // we hebben minstens een ouder en grootouder nodig om verder te kunnen werken
+        return;
+    }
+
+    // p van parent
+    Zoekboom<Sleutel, Data> *p = voorouders.top();
+    voorouders.pop();
+    // g van grandparent
+    Zoekboom<Sleutel, Data> *g = voorouders.top();
+
+    if((*p)->kleur == Kleur::Zwart){
+        //indien p zwart is moet er niets gebeuren. Want een nieuw toegevoegde rode knoop met zwarte ouder is ok.
+        std::cout << "p is zwart. Niets te doen"<< std::endl;
+        return;
+    }
+    bool pIsLeft = p->isLeftChildOfParent(*g);
+    bool cIsLeft = this->isLeftChildOfParent(*p);
+
+    // volgens p9 en volgende van de cursus
+
+    // situatie 1: g is zwart en heeft twee rode kinderen. Uiteraard is het kleinkind ook rood.
+    // oplossing: g wordt rood en zijn kinderen zwart.
+    // TO DO: kan  (*(*g)->links)->kleur een segmentation fault werpen??
+
+    // ik denk dat je niet moet controleren of G zwart is, volgens de cursus is dit altijd het geval
+    // ((*g)->kleur == Kleur::Zwart) 
+    if(getColorOfBrother(*g) == Kleur::Rood ){
+        std::cout << "situatie 1: b(" << (*(*g)->geefKind(!pIsLeft)).sleutel << ") is rood"<< std::endl;
+
+        (*(*g)->geefKind(!pIsLeft)).kleur = Kleur::Zwart;
+        (*p)->kleur = Kleur::Zwart;
+        (*g)->kleur = Kleur::Rood;
+        if(!pIsLeft){
+        std::cout << "b(" << (*(*g)->geefKind(!pIsLeft)).sleutel << ") is links kind and will be black" << std::endl;
+        }
+        else {
+        std::cout << "b(" <<  (*(*g)->geefKind(!pIsLeft)).sleutel << ") is rechts kind and will be black" << std::endl;
+        }
+        
+        std::cout << "p(" << (*p)->sleutel << ") will be black" << std::endl;
+        std::cout << "g(" << (*g)->sleutel << ") will be red" << std::endl;
+    } else {
+        // situatie 3: De broer b van p is zwart
+        // oplossing: roteer in de richting van de zwarte broer.
+        if((pIsLeft && !cIsLeft) || (!pIsLeft && cIsLeft)){ //indien p rechterkind is en c linkerkind (of spiegelbeeld)
+            std::cout << "situatie 3: b is zwart"<< std::endl;
+            // stel p is linkerkind. Dus geefLinkerkind en dit gaan we in de andere richting roteren als p is. Dus roteer naar rechts = false
+            (*g)->geefKind(pIsLeft).roteer(!pIsLeft); 
+            
+            
+        }
+        std::cout << "situatie 2: b is zwart"<< std::endl;
+        g->roteer(cIsLeft);
+        (*p)->kleur = Kleur::Zwart;
+        (*g)->kleur = Kleur::Rood;
+       
+    }
+
+    if( !repOK()){
+        fixBoom(voorouders);
+    }else {
+        std::cout << "rep oke, next" << std::endl;
+    }
+  
+  
+
+
+}
+
+/*
+*   Sommige operaties zijn geïnteresseerd in de voorouder geschiedenis, andere niet. Optionele parameters zijn niet mogelijk in cpp
+*   dus heb ik ervoor gekozen om de vooroudergeschiedenis te returnen. Dit kan niet als reference in tegenstelling tot wanneer we met een
+*   parameter hadden gewerkt. We declareren namelijk voorouders lokaal (binnen de scope van de functie), wanneer de functie eindigd zal deze
+*   variabele opgeruimd worden. We moeten dus het volledige object returnen. Wat je wel kan doen is voorouders = move(zoek(...)) doen ookal
+*   ik vermoed dat de compiler dat automatisch doet aangezien die ook wel weet dat het obj ten einde is.
+*/
+template <class Sleutel, class Data>
+stack<Zoekboom<Sleutel, Data> *> Zoekboom<Sleutel, Data>::zoek(const Sleutel &sleutel, zoekKnoop<Sleutel, Data> *&ouder, Zoekboom<Sleutel, Data> *&plaats)
+{
+    stack<Zoekboom<Sleutel, Data> *> voorouders;
     plaats = this;
     ouder = 0;
     while (*plaats && (*plaats)->sleutel != sleutel)
     {
         ouder = plaats->get();
+        voorouders.push(plaats);
         if ((*plaats)->sleutel < sleutel)
             plaats = &(*plaats)->rechts;
         else
             plaats = &(*plaats)->links;
     };
+    return voorouders;
+};
+template <class Sleutel, class Data>
+bool Zoekboom<Sleutel, Data>::isLeftChildOfParent( Zoekboom<Sleutel, Data> &p){
+    return (p->links == *this);
+}
+
+template <class Sleutel, class Data>
+Kleur Zoekboom<Sleutel, Data>::getColorOfBrother(Zoekboom<Sleutel, Data> &p){
+   
+    if(p->geefKind(!this->isLeftChildOfParent(p))){ //indien het andere kind bestaat vragen we zijn kleur op
+        return p->geefKind(!this->isLeftChildOfParent(p))->kleur;
+    } else {
+        // als het niet bestaat is het en virtuele knoop en is deze dus rood.
+        return Kleur::Rood;
+    }
+    
+    
 };
 
-/* eigen implementatie */
+template <class Sleutel, class Data>
+bool Zoekboom<Sleutel, Data>::repOK() const{
+ return repOK_blackRoot() && repOK_redParentWithBlackChilds();
+};
+   
+
+   
+
+template <class Sleutel, class Data>
+bool  Zoekboom<Sleutel, Data>::repOK_redParentWithBlackChilds() const{
+    bool statusOk = true;
+    if((*this)->kleur == Kleur::Rood){
+        if((*this)->links) {
+            if((*this)->links->kleur == Kleur::Rood){
+                statusOk = false;}
+            else {
+                statusOk = (*this)->links.repOK_redParentWithBlackChilds();
+            }
+        }
+         if((*this)->rechts) {
+            if((*this)->rechts->kleur == Kleur::Rood){
+                statusOk = false;}
+            else {
+                statusOk = (*this)->rechts.repOK_redParentWithBlackChilds();
+            }
+        }
+    }
+    return statusOk;
+
+};
+
+template <class Sleutel, class Data>
+bool  Zoekboom<Sleutel, Data>::repOK_blackRoot() const{
+  // root is altijd zwart
+    return ((*this)->kleur == Kleur::Zwart);
+};
 
 template <class Sleutel, class Data>
 int Zoekboom<Sleutel, Data>::geefDiepte()
@@ -251,29 +402,6 @@ void Zoekboom<Sleutel, Data>::roteer(bool naarRechts)
 
     // zet I als nieuwe parent
     *this = move(pointerToI);
-};
-
-template <class Sleutel, class Data>
-void Zoekboom<Sleutel, Data>::maakOnevenwichtig()
-{
-    // op een lege boom kunnen we geen operaties uitvoeren.
-    if (!*this)
-    {
-        return;
-    }
-    // als er een rechterkind is
-    if ((*this)->rechts)
-    {
-        // zolang er links kinderen zijn gaan we naar rechts roteren
-        while ((*this)->links)
-        {
-            (*this).roteer(true);
-        }
-        //recursief herhalen. Het is belangrijk dat je eerst alle kinderen van links naar rechts roteert. Als er namelijk linker kinderen zijn
-        //met een rechter kleinkind zouden deze niet weggeroteerd worden. Als we eerst roteren en dan pas recursief herhalen heb je dit probleem niet.
-        //snap je niet wat ik bedoel? wissel beide statements om en run het programma.
-        (*this)->rechts.maakOnevenwichtig();
-    }
 };
 
 #endif
