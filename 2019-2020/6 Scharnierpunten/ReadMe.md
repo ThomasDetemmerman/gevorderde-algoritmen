8d4b3c5a94a13aaa51b685a2fd123331bd850816
Thomas <thomas.detemmerman@ugent.be>
2019-10-30 14:16:59 +0100

    scharnierpunten afgewerkt

---
+++ 2019-2020/6 Scharnierpunten/ReadMe.md
@@ -0,0 +1,11 @@
+# Identificeren van scharnierpunten
+
+status: afgewerkt
+```
+preconditie: verbonden graaf
+0 is geen scharnierpunt want hij heeft maar 1 kind.
+3 is een scharnierpunt voor 4 (en zijn kinderen)
+2 is een scharnierpunt voor 3 (en zijn kinderen)
+1 is een scharnierpunt voor 2 (en zijn kinderen)
+```
+![output](./output.png)
--- 2019-2020/7 EulerCircuit/main.cpp
+++ 2019-2020/7 EulerCircuit/main.cpp
@@ -3,24 +3,32 @@
 using std::vector;
 
 void zoekScharnierpunten(Graaf<GERICHT> &graaf, int i);
-void zoekScharnierpuntenRec(Graaf <GERICHT> &graaf, int i, int hoofdwortel, vector<int> &laagsteTeBereiken,vector<int> &preOrderNummering, vector<bool> &ontdekt, vector<int> &parent);
-void controleerWortelOpScharnierpunten(Graaf<GERICHT> &graaf, int wortel);
+void zoekScharnierpuntenRec(Graaf <GERICHT> &graaf, int i, vector<int> &laagsteTeBereiken,vector<int> &preOrderNummering, vector<bool> &ontdekt, vector<int> &parent);
+
 
 int main() {
-    Graaf <GERICHT> graaf(6);
+    Graaf <GERICHT> graaf(7);
     // compontent A
-    std::cout << "preconditie: verbonden graaf" << std::endl;
-    graaf.voegVerbindingToe(0, 1);
     graaf.voegVerbindingToe(1,2);
-    graaf.voegVerbindingToe(2,3);
-    graaf.voegVerbindingToe(3,4);
-    graaf.voegVerbindingToe(4,5);
-    graaf.voegVerbindingToe(5,3);
+    graaf.voegVerbindingToe(2,1);
+    graaf.voegVerbindingToe(1,3);
+    graaf.voegVerbindingToe(3,2);
 
+    // compontent B
+    graaf.voegVerbindingToe(4,5);
+    graaf.voegVerbindingToe(5,4);
+    graaf.voegVerbindingToe(4,6);
+    graaf.voegVerbindingToe(6,4);
+
+    //brug 1
+    graaf.voegVerbindingToe(1,6);
+
+    //brug 2
+    graaf.voegVerbindingToe(0,5);
 
     graaf.teken("output.dot");
 
-    zoekScharnierpunten(graaf, 6);
+    zoekScharnierpunten(graaf, 7);
 
     return 0;
 }
@@ -32,27 +40,18 @@ void zoekScharnierpunten(Graaf <GERICHT> &graaf, int size){
     //volgens de theorie moet je eerst preorder nummeren en vervolgens bruggen zoeken. Deze twee kunnen echter wel gelijktijd.
     vector<int> preOrderNummering(size, -1);
     int num = 0;
-    controleerWortelOpScharnierpunten(graaf, 0);
-    zoekScharnierpuntenRec(graaf, 0, 0, laagsteTeBereiken, preOrderNummering, ontdekt, parent);
+    for(int i=0; i < size; i++){
+        zoekScharnierpuntenRec(graaf, i, laagsteTeBereiken, preOrderNummering, ontdekt, parent);
 
-
+    }
 
 }
 
-void controleerWortelOpScharnierpunten(Graaf<GERICHT> &graaf, int i){
-    if(graaf.operator[](i).size() > 1){
-        std::cout << i << " is een scharnierpunt want hij heeft meerdere kinderen." << std::endl;
-    } else {
-        std::cout << i << " is geen scharnierpunt want hij heeft maar 1 kind." << std::endl;
-    }
-};
-
-
-void zoekScharnierpuntenRec(Graaf <GERICHT> &graaf, int i, int hoofdwortel, vector<int> &laagsteTeBereiken,vector<int> &preOrderNummering, vector<bool> &ontdekt, vector<int> &parent){
+void zoekScharnierpuntenRec(Graaf <GERICHT> &graaf, int i, vector<int> &laagsteTeBereiken,vector<int> &preOrderNummering, vector<bool> &ontdekt, vector<int> &parent){
     ontdekt[i] = true;
-
-    preOrderNummering[i] = (i+1);   // je preorder nummer opslaan.
-    laagsteTeBereiken[i] = (i+1);   // je kan altijd jezelf bereiken.
+    i++;
+    preOrderNummering[i] = i;   // je preorder nummer opslaan.
+    laagsteTeBereiken[i] = i;   // je kan altijd jezelf bereiken.
 
     // we gaan nu aan elk van onze buren (kinderen) vragen of zij toevallig een betere knoop kunnen bereiken.
     for(auto const&[buurID, numverb]: graaf.operator[](i)){
@@ -60,7 +59,7 @@ void zoekScharnierpuntenRec(Graaf <GERICHT> &graaf, int i, int hoofdwortel, vect
         // als het nog niet ontdekt is gaan we dit recursief herhalen
         if(!ontdekt[buurID]){
             parent[buurID] = i;
-            zoekScharnierpuntenRec(graaf, buurID, hoofdwortel, laagsteTeBereiken, preOrderNummering, ontdekt, parent);
+            zoekScharnierpuntenRec(graaf, buurID, laagsteTeBereiken, preOrderNummering, ontdekt, parent);
 
             // We hebben het kind net de opdracht gegeven te berekenen welke knoop hij kan bereiken, we gaan nu zien of deze beter is als wat we al hebben
             //dit kind kan een lager genummerde knoop bereiken.
@@ -69,10 +68,7 @@ void zoekScharnierpuntenRec(Graaf <GERICHT> &graaf, int i, int hoofdwortel, vect
                 laagsteTeBereiken[i] = laagsteTeBereiken[buurID];
             } else {
                 if(laagsteTeBereiken[buurID] >= laagsteTeBereiken[i]){
-                    if(i != hoofdwortel) { //de hoofdwortel moet gecheckt worden met controleerWortelOpScharnierpunten()
-                        std::cout << i << " is een scharnierpunt voor " << buurID  << " (en zijn kinderen)" << std::endl;
-                    }
-
+                    std::cout << i << " is een scharnierpunt voor " << buurID << std::endl;
                 }
 
             }
