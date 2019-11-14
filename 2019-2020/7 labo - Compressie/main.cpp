78332085c787a0ea5c261994fae14953a284577b
thdtemme <Thomas.Detemmerman@UGent.be>
2019-11-14 17:38:48 +0100

    Update main.cpp

--- 2019-2020/7 labo - Compressie/main.cpp
+++ 2019-2020/7 labo - Compressie/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     decoderingstrie dt("../code.dat");
-    dt.teken("out.dot");
+    //dt.teken("out.dot");
     dt.decode("../TimErweten.cps", "../resultaat.txt");
     return 0;
 };
