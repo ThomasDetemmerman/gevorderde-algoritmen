756a1ad5506db750a678176f87126e8fc1a5b097
Thomas <thomas.detemmerman@ugent.be>
2019-12-04 21:00:09 +0100

    KMP geïmplementeerd.

---
+++ 2019-2020/C - Knuth-Morris-Pratt /KnuthMorrisPratt.h
@@ -0,0 +1,53 @@
+//
+// Created by Thomas on 04/12/2019.
+//
+
+#ifndef C___KNUTH_MORRIS_PRATT__KNUTHMORRISPRATT_H
+#define C___KNUTH_MORRIS_PRATT__KNUTHMORRISPRATT_H
+
+
+#include <queue>
+#include "prefix.h"
+
+class KnuthMorrisPratt {
+private:
+    Prefix prefixTabel;
+public:
+    KnuthMorrisPratt(Prefix pt);
+    queue<int> zoek(const string & tekst);
+};
+
+KnuthMorrisPratt::KnuthMorrisPratt(Prefix pt): prefixTabel(pt) {}
+
+queue<int> KnuthMorrisPratt::zoek(const string &tekst) {
+    queue<int> resultaten;
+    int Pc = 0; //patrooncounter
+    int Tc = 0; //tekstcounter
+
+    while(Tc < tekst.size()){
+
+
+        if(tekst[Tc] == prefixTabel.data[Pc]){
+            Tc++;
+            Pc++;
+        }
+        if(Pc == prefixTabel.data.size()){
+            resultaten.push(Tc-Pc);
+            Pc=0;
+            Tc++;
+        } else if (Tc < tekst.size() && tekst[Tc] != prefixTabel.data[Pc] ){
+            if(Pc == 0){
+                Tc++;
+            }
+            else {
+                //backtracking by using prefix function
+                Pc = prefixTabel[Pc-1];
+            }
+        }
+    }
+    return resultaten;
+};
+
+
+
+#endif //C___KNUTH_MORRIS_PRATT__KNUTHMORRISPRATT_H
---
+++ 2019-2020/C - Knuth-Morris-Pratt /prefix.h
@@ -0,0 +1,46 @@
+//
+// Created by Thomas on 04/12/2019.
+//
+
+#ifndef C___KNUTH_MORRIS_PRATT__PREFIX_H
+#define C___KNUTH_MORRIS_PRATT__PREFIX_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Prefix : public vector<int>{
+public:
+    string data;
+    Prefix(const string &zoekstring);
+
+    friend ostream& operator<<(ostream& os, const Prefix& dt);
+};
+
+Prefix::Prefix(const string &zoekstring) : data(zoekstring), vector<int>(zoekstring.length(),0){
+    for (int i = 1; i < zoekstring.length(); i++) {
+        int j = this->operator[](i-1);
+        while(j > 0 && zoekstring[i] != zoekstring[j]){
+            j = this->operator[](j-1);
+        }
+        if( zoekstring[i] == zoekstring[j]){
+            j++;
+            this->operator[](i) = j;
+        }
+    }
+}
+
+ostream& operator<<(ostream& os, const Prefix& dt)
+{
+    for (int i = 0; i < dt.size(); ++i) {
+        os << dt.data[i] << "\t";
+    }
+    os << endl;
+    for (int i = 0; i < dt.size(); ++i) {
+        os << dt[i]<< "\t";
+    }
+    return os;
+}
+
+#endif //C___KNUTH_MORRIS_PRATT__PREFIX_H
